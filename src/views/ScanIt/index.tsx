01b7fa750cd50210d116a556010704850e77f3ee
Reem Alon <reem.alon@lilibanking.com>
2023-09-25 20:38:17 +0300

    E2E upload image

--- src/views/ScanIt/index.tsx
+++ src/views/ScanIt/index.tsx
@@ -71,7 +71,7 @@ export const ScanIt = () => {
         <div className='flex flex-col grow h-full bg-white rounded-md items-center justify-center relative'>
             <Lottie animationData={AnimationData} play loop style={{position: "absolute", inset: 0, background: "#1f1f1f", borderRadius: '12px'}} />
             {!uploadedFiles.length && <div className='z-50'>
-            <h1 className='font-sans text-[48px] mb-[34px] text-white z-[999]'>{isClientLive ? 'Finish Upload' : 'Scan It'}</h1>
+            <h1 className='font-sans text-[48px] mb-[34px] text-white z-[999]'>{isClientLive ? 'Finish Upload' : 'Scan It.'}</h1>
             {qrImgData && !isClientLive && <div className='flex items-center justify-center w-[280px] h-[280px] rounded-[12px] color-change-2x border-[2px] border-white fade-In z-[999] mb-[24px]'>
             <img className='w-[230px] rounded-[12px] slide-in-left' src={qrImgData} alt='QR Code' />
             </div>}
